807a8be098b33241251dca3280666e72e50389b5
TTstacks <ekosh21.04.2007@gmail.com>
2023-06-26 14:58:38 +0600

    added new folder - fonts.

--- src/Constants/game_settings.h
+++ src/Constants/game_settings.h
@@ -3,6 +3,7 @@
 
 constexpr unsigned window_width = 700;
 constexpr unsigned window_height = 400;
+constexpr unsigned task_bar_height = 95; 
 constexpr unsigned half_window_width = window_width / 2;
 constexpr unsigned half_window_height = window_height / 2;
 #endif
